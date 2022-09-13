26d866cd6abc6a5233127c0e1fc0b398b2a7d194
praveen dodla <dodlapraveen09@gmail.com>
2022-09-13 18:22:43 +0530

    Creating Database with ADO.NET

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Payroll_ADO.NET
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Payroll Database");
+            EmployeePayrollDatabase.Create_Database();
+        }
+    }
+}
